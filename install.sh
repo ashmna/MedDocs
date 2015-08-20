03cdde50519168a65be52dc1ec2e123b6b5caa9f
ashmna <mnatsakanyan.ashot@gmail.com>
2015-08-21 02:58:46 +0400

    Added install scripts

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 php -r "readfile('https://getcomposer.org/installer');" | php
 php composer.phar install
 unlink ./skins/dev/global
-ln -s ./global ./skins/dev/global
+ln -s ${PWD}/global/ ./skins/dev/global
 cd ./global
 bower install ./bower.json
