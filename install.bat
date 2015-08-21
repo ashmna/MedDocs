a4458447dcddd6ca109a6c407be5f93a02e94565
ashmna <mnatsakanyan.ashot@gmail.com>
2015-08-21 16:41:30 +0400

    Install bat script bug fixed

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 php -r "readfile('https://getcomposer.org/installer');" | php
 php composer.phar install
-rmdir .\skins\dev\global
-mklink /J .\global .\skins\dev\global
-cd .\global
+rmdir skins\dev\global
+mklink /J skins\dev\global global
+cd global
 bower install .\bower.json
