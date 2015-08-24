6c0512513aae00d251176e9f91ef19869137065b
Armen <rmenhajian@gmail.com>
2015-08-24 22:21:24 +0400

    * doctor registration form improvements

--- global/templates/cloud2/pages/registration.php
+++ global/templates/cloud2/pages/registration.php
@@ -1,195 +1,8 @@
-<!-- Right sidebar start -->
-<div class="sb-slidebar sb-right sb-close">
-
-    <!-- User Profile Start -->
-    <section class="user-profile">
-        <div class="profile-container">
-            <img src="img/admin1.png" class="user-img" alt="User Image">
-
-            <div class="desc">
-                <h4>John Doe</h4>
-
-                <p class="text-info">UX Designer</p>
-            </div>
-        </div>
-        <ul class="ftr-link">
-            <li class="active">
-                <a href="javascript:;">
-                    <div class="fs1" aria-hidden="true" data-icon="&#xe033;"></div>
-                    985
-                </a>
-            </li>
-            <li>
-                <a href="javascript:;" class="text-success">
-                    <div class="fs1" aria-hidden="true" data-icon="&#xe0b9;"></div>
-                    999
-                </a>
-            </li>
-            <li>
-                <a href="javascript:;" class="text-pink">
-                    <div class="fs1" aria-hidden="true" data-icon="&#xe0e3;"></div>
-                    143
-                </a>
-            </li>
-        </ul>
-    </section>
-    <!-- User Profile End -->
-
-    <!-- Online Users start -->
-    <div class="block">
-        <div class="heading">Users Online</div>
-        <div class="wrapper">
-            <!-- Online Users Start -->
-            <ul class="online-users">
-                <li>
-                    <a href="#" data-lastname="Sandhya" data-status="online" data-original-title="">
-                        <span>Sunny</span>
-
-                        <div class="user-status online"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-lastname="Sandhya" data-status="online" data-original-title="">
-                        <span>Henrik</span>
-
-                        <div class="user-status online"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-lastname="Sandhya" data-status="online" data-original-title="">
-                        <span>John Doe</span>
-
-                        <div class="user-status busy"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-lastname="Sandhya" data-status="online" data-original-title="">
-                        <span>Fleming</span>
-
-                        <div class="user-status away"></div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-lastname="Sandhya" data-status="online" data-original-title="">
-                        <span>Wills</span>
-
-                        <div class="user-status"></div>
-                    </a>
-                </li>
-            </ul>
-            <!-- Online Users End -->
-        </div>
-    </div>
-    <!-- Online Users end -->
-
-    <!-- Checkout start -->
-    <div class="block">
-        <div class="heading">Checkout</div>
-        <ul class="cart">
-            <li class="cart-item">
-              <span class="cart-item-pic">
-                <img src="img/admin2.png" alt="EarthSquare">
-              </span>
-                Item #1
-                <span class="cart-item-desc">Description</span>
-                <span class="cart-item-price">$16.80</span>
-            </li>
-            <li class="cart-item">
-              <span class="cart-item-pic">
-                <img src="img/admin4.png" alt="EarthSquare">
-              </span>
-                Item #2
-                <span class="cart-item-desc">Description</span>
-                <span class="cart-item-price">$15.00</span>
-            </li>
-            <li class="cart-item">
-              <span class="cart-item-pic">
-                <img src="img/admin8.png" alt="EarthSquare">
-              </span>
-                Item #3
-                <span class="cart-item-desc">Description</span>
-                <span class="cart-item-price">$23.00</span>
-            </li>
-        </ul>
-        <div class="cart-bottom">
-            Total: $54.80
-            <a href="#" class="cart-button">Continue</a>
-        </div>
-    </div>
-    <!-- Checkout end -->
-
-    <!-- Notifications Start -->
-    <div class="block">
-        <div class="heading">Notifications</div>
-        <!-- Notifications -->
-        <ul class="infos">
-            <li>
-              <span class="label label-success pull-left">
-                <div class="fs1" aria-hidden="true" data-icon="&#xe128;"></div>
-              </span>
-
-                <div class="info">
-                    <h6>Guru Tweeted</h6>
-
-                    <p class="designation">Few seconds ago</p>
-                </div>
-            </li>
-            <li>
-              <span class="label label-danger pull-left">
-                <div class="fs1" aria-hidden="true" data-icon="&#xe0e9;"></div>
-              </span>
-
-                <div class="info">
-                    <h6>Server #999 Crashed</h6>
-
-                    <p class="designation">2 minutes ago</p>
-                </div>
-            </li>
-            <li>
-              <span class="label label-info pull-left">
-                <div class="fs1" aria-hidden="true" data-icon="&#xe0e3;"></div>
-              </span>
-
-                <div class="info">
-                    <h6>Kiri's account was created</h6>
-
-                    <p class="designation">5 days ago</p>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <!-- Notifications End -->
-
-</div>
-<!-- Right sidebar end -->
-
 <!-- Main Container Start -->
 <div class="main-container">
 
-    <!-- Top Bar Starts -->
-    <div class="top-bar clearfix">
-        <div class="page-title">
-            <h4>
-                <div class="fs1" aria-hidden="true" data-icon="&#xe0ab;"></div>
-                Form Elements
-            </h4>
-        </div>
-        <ul class="right-stats hidden-xs" id="mini-nav-right">
-            <li class="reportrange btn btn-success">
-                <i class="fa fa-calendar"></i>
-                <span></span> <b class="caret"></b>
-            </li>
-            <li>
-                <a href="#" class="btn btn-info sb-open-right sb-close">
-                    <div class="fs1" aria-hidden="true" data-icon="&#xe06a;"></div>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <!-- Top Bar Ends -->
-
     <!-- Container fluid Starts -->
-    <div class="container-fluid">
+    <div class="container-fluid" ng-controller="userController">
 
         <!-- Spacer starts -->
         <div class="spacer-xs">
@@ -198,7 +11,7 @@
                 <div class="col-md-6 col-sm-6 col-xs-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <h4>Horizontal Form</h4>
+                            <h4>Registrtation Form</h4>
                             <ul class="links">
                                 <li>
                                     <a href="#">
@@ -208,42 +21,89 @@
                             </ul>
                         </div>
                         <div class="panel-body">
-                            <form class="form-horizontal" role="form">
+                            <form class="form-horizontal" role="form" ng-submit="register(form)">
                                 <div class="form-group">
-                                    <label for="inputName" class="col-sm-2 control-label">Name</label>
+                                    <label class="col-sm-2 control-label"><?=_('First Name')?></label>
 
                                     <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputName" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3"
-                                               placeholder="Email">
+                                        <input type="text" class="form-control" placeholder="<?=_('First Name')?>" ng-model="registerInfo.firstName">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                                    <label class="col-sm-2 control-label"><?=_('Last Name')?></label>
 
                                     <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="inputPassword3"
-                                               placeholder="Password">
+                                        <input type="text" class="form-control" placeholder="<?=_('Last Name')?>" ng-model="registerInfo.lastName">
                                     </div>
                                 </div>
-                                <div class="form-group" style="margin-bottom: 2px;">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"> Remember me
-                                            </label>
-                                        </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=_('Patronymic Name')?></label>
+
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" placeholder="<?=_('Patronymic Name')?>" ng-model="registerInfo.patronymicName">
                                     </div>
                                 </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=_('Username')?></label>
+
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" placeholder="<?=_('Username')?>" ng-model="registerInfo.username">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=_('Gender')?></label>
+
+                                    <div class="col-sm-10">
+                                        <select class="form-control" ng-model="registerInfo.gender">
+                                            <option><?=_('Male')?></option>
+                                            <option><?=_('Female')?></option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=_('Birthday')?></label>
+
+                                    <div class="col-sm-10">
+                                        <fieldset class="birthday-picker" ng-model="registerInfo.firstName">
+                                            <select class='birth-year' name='birth[year]'></select>
+                                            <select class='birth-month' name='birth[month]'></select>
+                                            <select class='birth-day' name='birth[day]'></select>
+                                            <input type='hidden' name='birthdate' />
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=('Email')?></label>
+
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" placeholder="<?=('Email')?>" ng-model="registerInfo.email">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=('Phone')?></label>
+
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" placeholder="<?=('Phone')?>" ng-model="registerInfo.phone">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=('Address')?></label>
+
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" placeholder="<?=('Address')?>" ng-model="registerInfo.address">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label"><?=('Zip code')?></label>
+
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" placeholder="<?=('Zip code')?>" ng-model="registerInfo.zipCode">
+                                    </div>
+                                </div>
+
                                 <div class="form-group" style="margin-bottom: 0px;">
                                     <div class="col-sm-offset-2 col-sm-10">
-                                        <button type="submit" class="btn btn-success">Sign in</button>
+                                        <button type="submit" class="btn btn-success"><?=_('Register')?></button>
                                     </div>
                                 </div>
                             </form>
