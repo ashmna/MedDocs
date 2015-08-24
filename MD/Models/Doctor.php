c8fa596e384ee60e944ab7af7bbe4f27855343ef
ashmna <mnatsakanyan.ashot@gmail.com>
2015-08-24 21:43:24 +0400

    Change Doctor Model

--- MD/Models/Doctor.php
+++ MD/Models/Doctor.php
@@ -10,7 +10,19 @@ class Doctor extends Model {
     protected $partnerId;
     protected $userId;
 
+    protected $email;
+    protected $phone;
+    protected $firstName;
+    protected $lastName;
+    protected $patronymicName;
+    protected $gender = '';
+    protected $birthDay;
+    protected $address;
+    protected $zipCode;
+
     public function setUserId($userId) {
         $this->userId = $userId;
     }
+
+
 }
--- MD/Services/UserService.php
+++ MD/Services/UserService.php
@@ -6,4 +6,5 @@ namespace MD\Services;
 interface UserService {
     function login($username, $password, $rememberMe = false);
     function logout();
+    function register(array $userData);
 }
