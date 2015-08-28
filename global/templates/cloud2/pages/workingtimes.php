d5ec33c7b5052cb12448b4e3c3b4d081a76d216f
ashmna <mnatsakanyan.ashot@gmail.com>
2015-08-28 18:12:04 +0400

    workingTimesController table

--- global/templates/cloud2/pages/workingtimes.php
+++ global/templates/cloud2/pages/workingtimes.php
@@ -1,6 +1,16 @@
 <div ng-controller="workingTimesController" ng-cloak>
 
-    <table class="table" ng-init="renderMonth()">
+    <table border="1" ng-init="renderMonth()" class="g">
+
+        <tr>
+            <td><?= _('Mon') ?></td>
+            <td><?= _('Tue') ?></td>
+            <td><?= _('Wed') ?></td>
+            <td><?= _('Thu') ?></td>
+            <td><?= _('Fri') ?></td>
+            <td><?= _('Sat') ?></td>
+            <td><?= _('Sun') ?></td>
+        </tr>
 
         <tr ng-repeat="week in monthList track by $index">
             <td ng-repeat="day in week track by $index">
@@ -12,3 +22,22 @@
     </table>
 
 </div>
+
+
+<style>
+
+.g {
+    table-layout: fixed;
+    border-collapse: collapse;
+    width: 100%;
+}
+.g td {
+    padding: 0.25em 0.25em;
+    width: 14%;
+    height: 80px;
+    text-align: left;
+    vertical-align: top;
+}
+
+
+</style>
